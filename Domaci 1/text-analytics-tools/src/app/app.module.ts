2e56ce20d7260619263d7cc4a58348107699fb12
ZixHD <havos25@gmail.com>
2024-10-11 11:37:32 +0200

    working routes

---
+++ Domaci 1/text-analytics-tools/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './components/app/app.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { EntityExtractionComponent } from './components/entity-extraction/entity-extraction.component';
+import { TextSimilarityComponent } from './components/text-similarity/text-similarity.component';
+import { LanguageDetectionComponent } from './components/language-detection/language-detection.component';
+import { SentimentAnalysisComponent } from './components/sentiment-analysis/sentiment-analysis.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConfigurationComponent,
+    EntityExtractionComponent,
+    TextSimilarityComponent,
+    LanguageDetectionComponent,
+    SentimentAnalysisComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
